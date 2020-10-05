731ec9f764111b338360dc17b25454ab4233ae0a
Loanshark <63855173+changhyuni@users.noreply.github.com>
2020-10-05 19:08:35 +0900

    Update main.tf

--- multi_ec2/main.tf
+++ multi_ec2/main.tf
@@ -2,10 +2,17 @@ provider "aws" {
   region = "ap-northeast-2" 
 }
 
+resource "aws_key_pair" "ansible" {
+  key_name = "ansible"
+  public_key = file("./oooo.pub")
+}
+
 resource "aws_instance" "web" {
+  associate_public_ip_address = "true"
   ami = "ami-0a93a08544874b3b7"
   instance_type = "t2.micro"
-  key_name = "changman.pem"
+  key_name = aws_key_pair.oooo.key_name
+  subnet_id = "subnet-01a485fe30c67204e"
   count = "${var.instance_count}"
   tags = {
       Name = "myweb-${count.index + 1}"
